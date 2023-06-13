8b4656f79c01a1c91c89a9a609b4053ea258f8d3
Salvador Arnaldo Mellado Nuñez <salvador0796@gmail.com>
2023-06-13 00:38:12 -0400

    peticion crear usuario terminada

--- backend/models.py
+++ backend/models.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash
 from datetime import date
 
 
+
 # Modelos de las tablas de la db
 
 class Usuario:
@@ -21,10 +22,12 @@ class Usuario:
         self.correo = correo
         self.nombre_usuario = nombre_usuario
         self.contrasenia = generate_password_hash(password=contrasenia)
-        self.fecha_registro = date.today()
+        self.fecha_registro = date.today().strftime("%d/%m/%Y")
         self.is_admin = is_admin
         
-        
+    
+    def serializar_json(self):
+        return {'id': self.id, 'nombre': self.nombre, 'correo': self.correo, 'nombre_usuario': self.nombre_usuario, 'contrasenia': self.contrasenia, 'fecha_registro': self.fecha_registro, 'is_admin': str(self.is_admin)}
         
         
 
