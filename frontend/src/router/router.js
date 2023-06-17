68e28b1c7fb6c452de7724aaf1b9a9b42347a922
sebastian <fxzioneh@gmail.com>
2023-06-16 20:16:51 -0400

    f5

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';   // Librería de vue router importación
 import InicioComponent from '@/views/InicioComponent.vue';      // Importación del componente de inicio que está en la sección views llamada InicioComponent.vue
-import registrarComponent from '@/views/RegistrarComponent.vue';
-import LoginComponent from '@/views/LoginComponent.vue'
+import registrarComponent from '@/views/auth/RegistrarComponent.vue';
+import LoginComponent from '@/views/auth/LoginComponent.vue'
 import PanelComponent from '@/views/PanelComponent.vue'
 
 const routes = [   //Se crea una constante de rutas o routes,  donde adentro de las llaves contiene un array dónde contiene las rutas de cada componente ala que le llamamos.
